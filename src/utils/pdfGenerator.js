c7bfc702fc24fd084438b5b4c57532e680347b26
Shubham Kulkarni <shubhamkulkarni.sh@gmail.com>
2021-09-02 20:22:04 +0530

    new measurements

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -15,29 +15,29 @@ export const generatePdf = async (receiptData) => {
     format: "letter",
     // putOnlyUsedFonts:true
   });
-  doc.setFontSize(12);
+  doc.setFontSize(14);
   doc.setFont("Sarai_07");
   //   doc.addFont('Devanagari.ttf', 'LohitMarathi', 'normal');
 
   // doc.text(1, 3, 'शुभम');
-  doc.text(data.receiptNo, 5, 11.71);
-  doc.text(data.receiptNo, 18, 11.81);
-  doc.text(formattedDate(data.date), 12, 11.71);
-  doc.text(formattedDate(data.date), 25.2, 11.8);
+  doc.text(data.receiptNo, 5, 13.21);
+  doc.text(data.receiptNo, 18, 13.31);
+  doc.text(formattedDate(data.date), 12, 13.21);
+  doc.text(formattedDate(data.date), 25.2, 13.4);
 
   //   doc.text(data.amount, 4.1, 6.30)
   //   doc.text(data.amount, 10, 6.30)
-  doc.text(data.name, 4, 12.65);
-  doc.text(data.name, 17.15, 12.65);
+  doc.text(data.name, 4, 14.15);
+  doc.text(data.name, 17.15, 14.15);
 
-  doc.text(data.numInWords + " Only", 5.5, 14.45);
-  doc.text(data.numInWords + " Only", 18.5, 14.45);
+  doc.text(data.numInWords + " Only", 5.5, 16);
+  doc.text(data.numInWords + " Only", 18.5, 16);
 
-  doc.setFontSize(15);
-  doc.text(data.mobile.toString(), 4, 16.12);
-  doc.text(data.mobile.toString(), 17, 16.14);
-  doc.text(data.amount.toFixed(2).toString(), 11, 16.13);
-  doc.text(data.amount.toFixed(2).toString(), 24.2, 16.13);
+  doc.setFontSize(16);
+  doc.text(data.mobile.toString(), 4, 17.63);
+  doc.text(data.mobile.toString(), 17, 17.63);
+  doc.text(data.amount.toFixed(2).toString(), 11, 17.63);
+  doc.text(data.amount.toFixed(2).toString(), 24.2, 17.63);
 
   await doc.save(data.receiptNo + "_Receipt.pdf");
   NotificationManager.success(
